a7113d232906314072f5d4b0cb73532bd6c6a072
Francesco Rodríguez <frodsan@me.com>
2014-08-04 23:22:43 -0300

    Update code style.

--- lib/mote/render.rb
+++ lib/mote/render.rb
@@ -10,20 +10,22 @@ module Mote::Render
   end
 
   def render(template, locals = {}, layout = settings[:mote][:layout])
-    res.write view(template, locals, layout)
+    res.write(view(template, locals, layout))
   end
 
   def view(template, locals = {}, layout = settings[:mote][:layout])
-    partial(layout, locals.merge(content: partial(template, locals)))
+    return partial(layout, locals.merge(content: partial(template, locals)))
   end
 
   def partial(template, locals = {})
-    mote(mote_path(template), locals.merge(app: self), TOPLEVEL_BINDING)
+    return mote(mote_path(template), locals.merge(app: self), TOPLEVEL_BINDING)
   end
 
   def mote_path(template)
-    return template if template.end_with?(".mote")
-
-    File.join(settings[:mote][:views], "#{template}.mote")
+    if template.end_with?(".mote")
+      return template
+    else
+      return File.join(settings[:mote][:views], "#{template}.mote")
+    end
   end
 end
